8bb1c3b5c6efad0f4a118d85deff3238388848b6
Vaishu Chintam <jc103@wellesley.edu>
2024-04-29 20:40:21 -0400

    Added some PBT testing (Vaishu & Bella)

--- linkedList.py
+++ linkedList.py
@@ -1,3 +1,5 @@
+from hypothesis import given, strategies as st
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -57,10 +59,14 @@ class LinkedList:
     def deleteAtTail(self):
         if self.head is None:
             return
+        elif self.head.next is None:
+            self.head = None
+            return
         else:
             currentNode = self.head
-            while (currentNode.next.next):
+            while currentNode.next.next:
                 currentNode = currentNode.next
+            
             currentNode.next = None
 
     def deleteAtIndex(self, index):
@@ -81,3 +87,66 @@ class LinkedList:
                         currentIndex += 1
         
                 print("Invalid index entered")
+    
+    def convertToList(self):
+        result = []
+        currentNode = self.head
+        while currentNode:
+            result.append(currentNode.data)
+            currentNode = currentNode.next
+        return result
+
+
+@given(st.lists(st.integers()))
+def test_insertAtHead(values):
+    linkedList = LinkedList()
+    
+    for value in values:
+        linkedList.insertAtHead(value)
+
+    assert linkedList.convertToList() == list(reversed(values))
+
+@given(st.lists(st.integers()))
+def test_insertAtTail(values):
+    linkedList = LinkedList()
+
+    for value in values:
+        linkedList.insertAtTail(value)
+
+    assert linkedList.convertToList() == values
+
+@given(st.lists(st.integers()), st.integers(min_value=1))
+def test_deleteAtHead(values, n):
+    linkedList = LinkedList()
+
+    for value in values:
+        linkedList.insertAtTail(value)
+
+    numOfDeletes = min(n, len(values)) # don't delete more times than the length of the list
+    for _ in range(numOfDeletes): 
+        linkedList.deleteAtHead()
+    assert linkedList.convertToList() == values[numOfDeletes:]
+
+@given(st.lists(st.integers()), st.integers(min_value=1))
+def test_deleteAtTail(values, n):
+    linkedList = LinkedList()
+
+    for value in values:
+        linkedList.insertAtTail(value)
+
+    numOfDeletes = min(n, len(values)) # don't delete more times than the length of the list
+    for _ in range(numOfDeletes): 
+        linkedList.deleteAtTail()
+
+    assert linkedList.convertToList() == values[:len(values) - numOfDeletes]
+
+
+def main():
+    test_insertAtHead()
+    test_insertAtTail()
+    test_deleteAtHead()
+    test_deleteAtTail()
+    print("All tests passed")
+
+if __name__ == "__main__":
+    main()
